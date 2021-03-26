bcc8d2635edbb0173ed74b74b487137e3f81c528
Hunter Wells <haw2048@rit.edu>
2021-03-26 03:36:47 -0400

    added very rough fsm

--- SolsUnderground/Game1.cs
+++ SolsUnderground/Game1.cs
@@ -6,10 +6,25 @@ using Microsoft.Xna.Framework.Input;
 
 namespace SolsUnderground
 {
+    public enum GameState
+    {
+        Menu,
+        Controls,
+        Instructions,
+        Game,
+        Pause,
+        GameOver
+            
+
+    }
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private GameState currentState;
+        private SpriteFont heading;
+        private SpriteFont buttonText;
+        private SpriteFont text;
 
         public Game1()
         {
@@ -21,13 +36,16 @@ namespace SolsUnderground
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            currentState = GameState.Menu;
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            heading = Content.Load<SpriteFont>("Roboto175");
+            buttonText = Content.Load<SpriteFont>("Roboto100");
+            text = Content.Load<SpriteFont>("Roboto40");
 
             // TODO: use this.Content to load your game content here
         }
@@ -36,6 +54,36 @@ namespace SolsUnderground
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            KeyboardState kb = Keyboard.GetState();
+
+            switch(currentState)
+            {
+                case GameState.Menu:
+                    if (kb.IsKeyDown(Keys.I))
+                        currentState = GameState.Instructions;
+                    if (kb.IsKeyDown(Keys.C))
+                        currentState = GameState.Controls;
+                    if (kb.IsKeyDown(Keys.Enter))
+                        currentState = GameState.Game;
+                        break;
+                case GameState.Controls:
+                    if(kb.IsKeyDown(Keys.Escape))
+                        currentState = GameState.Menu;
+                    break;
+                case GameState.Instructions:
+                    if (kb.IsKeyDown(Keys.Escape))
+                        currentState = GameState.Menu;
+                    break;
+                case GameState.Game:
+                    if (kb.IsKeyDown(Keys.Escape))
+                        currentState = GameState.Pause;
+                    break;
+                case GameState.GameOver:
+                    if (kb.IsKeyDown(Keys.Enter))
+                        currentState = GameState.Game;
+                    break;
+
+            }
 
             // TODO: Add your update logic here
 
@@ -45,8 +93,9 @@ namespace SolsUnderground
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            
             // TODO: Add your drawing code here
+
 
             base.Draw(gameTime);
         }
